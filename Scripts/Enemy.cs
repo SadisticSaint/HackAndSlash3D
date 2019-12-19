a7416ca804c287b55a49ef06a69eb7142e339dde
Mattthew Williams <williamsmd90@gmail.com>
2019-12-19 17:00:19 -0500

    moved Cartoon FX to art folder, added hit and death animation for enemy, modified Enemy script

--- Scripts/Enemy.cs
+++ Scripts/Enemy.cs
@@ -2,6 +2,12 @@
 
 public class Enemy : MonoBehaviour, ITakeDamage
 {
+    [SerializeField]
+    private GameObject impactParticle;
+    [SerializeField]
+    private int maxHealth = 3;
+
+    private int currentHealth;
     private Animator animator;
 
     private void Awake()
@@ -9,7 +15,23 @@ public class Enemy : MonoBehaviour, ITakeDamage
         animator = GetComponentInChildren<Animator>();
     }
 
+    private void OnEnable()
+    {
+        currentHealth = maxHealth;
+    }
+
     public void TakeDamage(Character hitBy)
+    {
+        currentHealth--;
+        Instantiate(impactParticle, new Vector3(0, 2, 0), Quaternion.identity);
+
+        if (currentHealth <= 0)
+            Die();
+        else
+            animator.SetTrigger("Hit");
+    }
+
+    private void Die()
     {
         animator.SetTrigger("Die");
         Destroy(gameObject, 2f);
