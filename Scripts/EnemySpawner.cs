25d199af8043aae17bee349532655aec46a0a9f9
Mattthew Williams <williamsmd90@gmail.com>
2019-12-21 15:23:56 -0500

    added Enemy spawner to Level01, added EnemySpawner script

---
+++ Scripts/EnemySpawner.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private Enemy[] enemyPrefabs;
+    [SerializeField]
+    private Transform[] spawnPoints;
+    [SerializeField]
+    private float respawnRate = 10f;
+    [SerializeField]
+    private float initialSpawnDelay;
+    [SerializeField]
+    private int totalNumberToSpawn;
+    [SerializeField]
+    private int numberToSpawnEachTime = 1;
+
+    private float spawnTimer;
+
+    private void Update()
+    {
+        spawnTimer += Time.deltaTime;
+        if (ShouldSpawn())
+            Spawn();
+    }
+
+    private bool ShouldSpawn()
+    {
+        return spawnTimer >= respawnRate;
+    }
+
+    private void Spawn()
+    {
+        spawnTimer = 0f;
+
+        Enemy prefab = ChooseRandomEnemyPrefab();
+
+        if(prefab != null)
+        {
+            Transform spawnPoint = ChooseRandomSpawnPoint();
+            var enemy = Instantiate(prefab, spawnPoint.position, spawnPoint.rotation);
+        }
+    }
+
+    private Transform ChooseRandomSpawnPoint()
+    {
+        if (spawnPoints.Length == 0)
+            return transform;
+        if (spawnPoints.Length == 1)
+            return spawnPoints[0];
+
+        int index = UnityEngine.Random.Range(0, spawnPoints.Length);
+        return spawnPoints[index];
+    }
+
+    private Enemy ChooseRandomEnemyPrefab()
+    {
+        if (enemyPrefabs.Length == 0)
+            return null;
+        if (enemyPrefabs.Length == 1)
+            return enemyPrefabs[0];
+
+        int index = UnityEngine.Random.Range(0, enemyPrefabs.Length);
+        return enemyPrefabs[index];
+    }
+}
