650507d86f0324451582b97d23862af8b8ca256d
Mattthew Williams <williamsmd90@gmail.com>
2019-12-22 21:17:39 -0500

    added gizmos to EnemySpawner script

--- Scripts/EnemySpawner.cs
+++ Scripts/EnemySpawner.cs
@@ -87,4 +87,17 @@ public class EnemySpawner : MonoBehaviour
         int index = UnityEngine.Random.Range(0, enemyPrefabs.Length);
         return enemyPrefabs[index];
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawCube(transform.position, Vector3.one);
+
+        foreach (var spawnPoint in spawnPoints)
+        {
+            Gizmos.DrawSphere(spawnPoint.position, 0.5f);
+        }
+    }
+#endif
 }
