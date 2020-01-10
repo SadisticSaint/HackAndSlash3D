29138956912780dd871a68beff456f3b0694128e
Mattthew Williams <williamsmd90@gmail.com>
2020-01-10 15:14:54 -0500

    added Pool folder with PooledMonoBehaviour and Pool scripts, started object pooling

---
+++ Scripts/Pooling/Pool.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pool : MonoBehaviour
+{
+    private Queue<PooledMonoBehaviour> objects = new Queue<PooledMonoBehaviour>();
+
+    private PooledMonoBehaviour prefab;
+
+    public T Get<T>() where T : PooledMonoBehaviour
+    {
+        if(objects.Count == 0)
+        {
+            GrowPool();
+        }
+
+        var pooledObject = objects.Dequeue();
+        return pooledObject as T;
+    }
+
+    private void GrowPool()
+    {
+        for (int i = 0; i < prefab.InitialPoolSize; i++)
+        {
+            var pooledObject = Instantiate(prefab) as PooledMonoBehaviour; //why cast this?
+            pooledObject.gameObject.name += string.Empty + i;
+
+            pooledObject.OnReturnToPool += AddObjectToAvailableQueue;
+
+            pooledObject.transform.SetParent(this.transform); //makes all pooled objects children of the pool to make easier to find?
+            pooledObject.gameObject.SetActive(false);
+        }
+    }
+
+    private void AddObjectToAvailableQueue(PooledMonoBehaviour pooledObject)
+    {
+        pooledObject.transform.SetParent(this.transform);
+        objects.Enqueue(pooledObject);
+    }
+}
---
+++ Scripts/Pooling/PooledMonoBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+public class PooledMonoBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    private int initialPoolSize = 50;
+
+    public event Action<PooledMonoBehaviour> OnReturnToPool;
+
+    public int InitialPoolSize { get { return initialPoolSize; } }
+
+    private void OnDisable()
+    {
+        if (OnReturnToPool != null)
+            OnReturnToPool(this);
+    }
+}
