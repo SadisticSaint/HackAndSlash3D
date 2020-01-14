a256e6fb5df1e1f6345d70606204e1d93003c2fb
Mattthew Williams <williamsmd90@gmail.com>
2020-01-13 21:33:27 -0500

    added DeathParticles script for player death particle effect

---
+++ Scripts/DeathParticles.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class DeathParticles : MonoBehaviour
+{
+    [SerializeField]
+    private PooledMonoBehaviour deathParticlePrefab;
+
+    private Character character;
+
+    private void Awake()
+    {
+        character = GetComponent<Character>();
+    }
+
+    private void OnEnable()
+    {
+        character.OnDied += Character_OnDied;
+    }
+
+    private void OnDisable()
+    {
+        character.OnDied -= Character_OnDied;
+    }
+
+    private void Character_OnDied(Character character)
+    {
+        character.OnDied -= Character_OnDied;
+        deathParticlePrefab.Get<PooledMonoBehaviour>(transform.position, Quaternion.identity);
+    }
+}
